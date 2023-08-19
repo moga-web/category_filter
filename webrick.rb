a46b3145c67393f3fed11bb178716452a4143706
mogami <ndarfc.57@gmail.com>
2023-08-19 22:15:27 +0900

    add form

--- webrick.rb
+++ webrick.rb
@@ -1,4 +1,4 @@
- # webrick.rb
+# webrick.rb
 require 'webrick'
 
 server = WEBrick::HTTPServer.new({ 
@@ -14,6 +14,28 @@ trap(:INT){
 server.mount_proc("/time") do |req, res|
   # レスポンス内容を出力
   body = "<html><body>#{Time.now}</body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
+server.mount_proc("/form_get") do |req, res|
+  req.query
+  body = "<html><head><meta charset='utf-8'></head><body>\n"
+  body += "クエリパラメーターは#{req.query}です<br>"
+  body += "こんにちは#{req.query['username']}さん。あなたの年齢は#{req.query['age']}ですね"
+  body += "</body</html>\n"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
+server.mount_proc("/form_post") do |req, res|
+  req.query
+  body = "<html><head><meta charset='utf-8'></head><body>\n"
+  body += "クエリパラメーターは#{req.query}です<br>"
+  body += "こんにちは#{req.query['username']}さん。あなたの年齢は#{req.query['age']}ですね"
+  body += "</body</html>\n"
   res.status = 200
   res['Content-Type'] = 'text/html'
   res.body = body
